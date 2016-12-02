add5f8de01ecf5bcaa185400dca4b305fd211eed
Lgfei <18664571731@163.com>
2016-12-02 17:09:16 +0800

    【提交人】：Lgfei 【提交内容】：删除不适合存放在该仓库的代码 【提交时间】：2016-12-2

--- mysite/src/com/lgfei/mysite/service/IUserService.java
+++
@@ -1,9 +0,0 @@
-package com.lgfei.mysite.service;
-
-import com.lgfei.mysite.message.ResultMessageVO;
-import com.lgfei.mysite.vo.UserInfoVO;
-
-public interface IUserService {
-
-	public ResultMessageVO login(UserInfoVO user);
-}
